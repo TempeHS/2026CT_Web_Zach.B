de761e1a44bd88a7e4c8da2e201f2365b9f17105
Zachary B <zachary.booth3@education.nsw.gov.au>
2025-05-05 02:49:25 +0000

    create navbar template

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
+@app.route('/index.html')
 @app.route('/')
-def hello_world():
-    back_end_data = "no one"
-    return render_template("index.html",front_end_data = back_end_data), 200
+def index():
+    return render_template("index.html"), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
