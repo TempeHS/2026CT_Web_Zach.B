cdda49e46be18ba5bfcfd617ecb40256787f8613
Zachary B <zachary.booth3@education.nsw.gov.au>
2025-05-21 01:54:52 +0000

    card images added

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 @app.route('/index.html')
 @app.route('/')
 def index():
-    return render_template("index.html"), 200
+    card_data = (
+        ("Factory", "The factory gamemode", "Check it out" "static/images/card_images/card_factory.png"),
+        ("Tower of doom", "The Tower of Doom gamemode", "Check it out" "static/images/card_images/card_towerofdoom.png"),
+        ("Fishing", "The fishing gamemode", "Check it out" "static/images/card_images/card_fishing.png"),
+    )
+    return render_template("index.html", cards=card_data), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
